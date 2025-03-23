89cdf56fda6d16d00dfe07c5ac69b503be8ecd72
ian <iangurgel25@hotmail.com>
2025-03-23 13:21:04 -0300

    criando pagina do carrinho

--- src/app/componentes/Carrinho_Article.tsx
+++ src/app/componentes/Carrinho_Article.tsx
@@ -7,10 +7,10 @@ import { useState } from 'react';
 
 export default function Carrinho_Article() {
     return (
-        <article className="w-full h-fitt bg-white rounded-md overflow-hidden flex flex-col justify-between">
+        <article className="w-full h-fitt bg-white rounded-md overflow-hidden flex flex-col md:flex-row justify-between">
             <img src="https://placehold.co/600x400" alt="" className="w-full h-[250px] object-center object-cover" />
 
-            <div className="flex flex-col justify-between grow px-2">
+            <div className="flex flex-col justify-between grow px-3">
                 <div className="min-h-[125px]">
                     <div className="flex items-center justify-between my-2">
                         <h3 className="text-2xl font-semibold">Nome do Produto</h3>
@@ -20,15 +20,14 @@ export default function Carrinho_Article() {
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet atque voluptas cupiditate! Repellat dolorem alias error sit, rerum laboriosam. Fuga.</p>
                 </div>
-                <div className="flex justify-between my-2">
+                <div className="flex justify-between my-3">
                     <div className="flex gap-x-2 items-center bg-gray-300 outline-2 outline-gray-400 px-4 py-1 rounded-lg cursor-pointer"
-                    
+        
                     >
                         <span>1</span>
                         <span>
                             <RxCaretDown />
-                            {/* <RxCaretUp /> */}
-
+                        
                         </span>
                     </div>
                     <span className="font-bold text-lg">R$ 40,00</span>
